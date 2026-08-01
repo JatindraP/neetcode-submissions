db21e030f0c15ce2f4e77d5a61d4512d7824ad1e
JatindraP <46838410+JatindraP@users.noreply.github.com>
2026-08-01 19:57:55 +0530

    Add: string-encode-and-decode - submission-9

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-9.py
@@ -0,0 +1,13 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if not strs:
+            return '[]'
+        return '~'.join(strs)
+
+    def decode(self, s: str) -> List[str]:
+        if s == '[]':
+            return []
+        strs = s.split('~')
+        return strs
+        
