e4fa73ab208562402d66a64b5242bd320221da0b
JatindraP <46838410+JatindraP@users.noreply.github.com>
2026-08-05 08:23:09 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    seen = [-1]*(45+1)
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        if self.seen[n-1] == -1:
+            self.seen[n-1] = self.climbStairs(n-1)
+        if self.seen[n-2] == -1:
+            self.seen[n-2] = self.climbStairs(n-2)
+        return self.seen[n-1] + self.seen[n-2]
